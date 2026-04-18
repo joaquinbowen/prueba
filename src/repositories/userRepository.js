a1a889256a3f4836c622e0eaad2fff6e8fa0e8c2
Didier Guaña Patiño <bowenjoaquinsd@gmail.com>
2026-04-18 14:33:58 -0500

    aaaaa

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -9,7 +9,15 @@ async function obtenerPorEmail(email) {
     return await prisma.usuario.findUnique({ where: { email } })
 }
 
+async function cambiarPremium(id) {
+    return await prisma.usuario.update({
+        where:{id:id},
+        data:{rol:"premium"}
+    })
+}
+
 module.exports = {
     crearUser,
-    obtenerPorEmail
+    obtenerPorEmail,
+    cambiarPremium
 }
