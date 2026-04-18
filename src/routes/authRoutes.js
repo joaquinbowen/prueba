0fa8305fa0df278b551b1e4cb50c9be4a90558f8
Didier Guaña Patiño <bowenjoaquinsd@gmail.com>
2026-04-18 14:40:38 -0500

    rrrr

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,5 +7,6 @@ const authController = require("../controllers/authController");
 router.post("/registrar", authController.registrarUsuario);
 router.post("/login", authController.loginUsuario);
 router.post("/logout",authController.logout);
+router.put("/users/premium",authController.cambiarPremium)
 
 module.exports = router
